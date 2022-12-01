c7078998bcb59a69841786146504e4705c77d8f2
Toni <2703233+toniengelhardt@users.noreply.github.com>
2022-12-01 17:56:51 +0000

    Add vercel build config

---
+++ vercel-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Explanation:
+# https://vercel.com/support/articles/how-do-i-use-the-ignored-build-step-field-on-vercel
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
