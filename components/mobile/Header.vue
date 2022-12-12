93c5edf9d19e7a04adf8ded04a5972cff5b38505
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-12-12 21:03:43 +0000

    Added wallet, gas and block# for mob-header

--- components/mobile/Header.vue
+++ components/mobile/Header.vue
@@ -5,7 +5,17 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>
-    Header
+  <div w-full>
+    <div col-span-3 flex h-full items-center>
+      <div w-full pl-2>
+        <GasPrice />
+      </div>
+      <div w-full pl-1 pr-1>
+        <Wallet />
+      </div>
+      <div w-full pr-2>
+        <NetworkInfo />
+      </div>
+    </div>
   </div>
 </template>
