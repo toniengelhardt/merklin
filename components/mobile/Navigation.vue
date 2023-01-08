1b03a73e55dca8dcba1a834d53a5d1c9db3b304d
Toni <2703233+toniengelhardt@users.noreply.github.com>
2023-01-08 22:22:16 +0000

    Fix safe-area-inset for iPhone

--- components/mobile/Navigation.vue
+++ components/mobile/Navigation.vue
@@ -1,5 +1,9 @@
 <template>
-  <div flex justify-around items-center w-full h-14 px-3 bg-base box-border border-t-solid border-base>
+  <div
+    class="mobile-navigation"
+    flex justify-around items-center w-full px-3 bg-base
+    box-border border-t-solid border-t-1 border-base
+  >
     <AppMenuItemMobile
       v-for="(item, idx) in appMenuItems"
       :key="idx"
@@ -8,3 +12,10 @@
     <AccountMenu direction="up" toggle-class="text-dim" />
   </div>
 </template>
+
+<style scoped>
+.mobile-navigation {
+  height: calc(3.5rem + env(safe-area-inset-bottom));
+  padding-bottom: env(safe-area-inset-bottom);
+}
+</style>
